a53f2abd3476716c32d173df34b33bb343b36948
ahmedfehor <ahmedfehor@gmail.com>
2025-07-31 09:55:40 +0600

    fav icon added

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/frontend/images/favicon.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/frontend/images/favicon.jpeg') }}">
 
     <!-- CSS
  ============================================ -->
